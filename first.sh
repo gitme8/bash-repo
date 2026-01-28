80b8c25f11360248751e4f5749a25a89aa6842e2
Pavan <gitme8@github.com>
2026-01-28 19:24:42 +0530

    new

--- first.sh
+++ first.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-a=$1
-b=$2
-c=$3
-if [ $a -eq "apple" ] && [ $b -eq "banana" ] && [ $c -eq "cherry" ]; then
+
+a="$1"
+b="$2"
+c="$3"
+
+if [[ "$a" == "apple" && "$b" == "banana" && "$c" == "cherry" ]]; then
   echo "Fruits matched!"
 else
   echo "Fruits did not match."
 fi
+
